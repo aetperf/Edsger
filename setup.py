7a537d3e2a6817af5f40d6af13bd2e796192f37a
djfrancesco <pacullfrancois@gmail.com>
2022-12-28 09:38:52 +0100

    fixed a typo

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ extra_compile_args = ["-Ofast"]
 
 extensions = [
     Extension(
-        "edsger.pqueue_imp_bin_dec_1b",
-        ["src/edsger/pqueue_imp_bin_dec_1b.pyx"],
+        "edsger.commons",
+        ["src/edsger/commons.pyx"],
+        extra_compile_args=extra_compile_args,
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+    Extension(
+        "edsger.pqueue_imp_bin_dec_0b",
+        ["src/edsger/pqueue_imp_bin_dec_0b.pyx"],
         extra_compile_args=extra_compile_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
@@ -20,7 +26,7 @@ setup(
     ext_modules=cythonize(
         extensions,
         compiler_directives={"language_level": "3"},
-        include_path=["src/edsger/", "tests/"],
+        include_path=["src/edsger/"],
     ),
     include_dirs=[np.get_include()],
 )
