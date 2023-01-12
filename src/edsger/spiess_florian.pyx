a164a36db2ab5d7b5f98660422150d628a76df38
djfrancesco <pacullfrancois@gmail.com>
2023-01-12 15:18:36 +0100

    new file

---
+++ src/edsger/spiess_florian.pyx
@@ -0,0 +1,10 @@
+""" An implementation of Spiess and Florian's hyperpath generating algorithm.
+
+Spiess, H. and Florian, M. (1989). Optimal strategies: A new assignment model 
+for transit networks. Transportation Research Part B 23(2), 83-102.
+    
+author : Francois Pacull
+copyright : Architecture & Performance
+email: francois.pacull@architecture-performance.fr
+license : MIT
+"""
