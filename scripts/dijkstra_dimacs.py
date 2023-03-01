06d95e4e7bcc94549d60bda38de0691fa472fac6
djfrancesco <pacullfrancois@gmail.com>
2023-03-01 22:20:08 +0100

    wip

--- scripts/dijkstra_dimacs.py
+++ scripts/dijkstra_dimacs.py
@@ -48,6 +48,16 @@ parser.add_argument(
     required=True,
 )
 parser.add_argument(
+    "-l",
+    "--library",
+    dest="library_name",
+    help='library name, must be "E" (Edsger), "GT" (graph-tool), "NK" (NetworKit), "RX" (Rustworkx)',
+    metavar="TXT",
+    type=str,
+    required=False,
+    default="E",
+)
+parser.add_argument(
     "-f",
     "--from",
     dest="idx_from",
@@ -85,6 +95,10 @@ reg = reg.upper()
 idx_from = args.idx_from
 repeat = args.repeat
 check_result = args.check_result
+lib = args.library_name.upper()
+
+# lib name check
+assert lib in ["E", "GT", "NK", "RX"]
 
 data_dir_found = os.path.exists(data_dir)
 if data_dir_found:
@@ -148,37 +162,222 @@ edge_count = len(edges)
 vertex_count = edges[["tail", "head"]].max().max() + 1
 logger.info(f"{edge_count} edges and {vertex_count} vertices")
 
+if lib == "E":
+    # Edsger
+    # ------
 
-# Edsger
+    logger.info("Edsger init")
+    edges[["tail", "head"]] = edges[["tail", "head"]].astype(np.uint32)
+    sp = Dijkstra(edges, orientation="out", check_edges=False)
 
-logger.info("Edsger init")
-edges[["tail", "head"]] = edges[["tail", "head"]].astype(np.uint32)
-sp = Dijkstra(edges, orientation="out", check_edges=False)
+    # SSSP
 
-results = []
-logger.info("Edsger Run")
-for i in range(repeat):
-    d = {}
+    results = []
+    logger.info("Edsger Run")
+    for i in range(repeat):
+        d = {}
 
-    start = perf_counter()
+        start = perf_counter()
 
-    path_lengths = sp.run(vertex_idx=idx_from, return_inf=True)
-    dist_matrix = path_lengths.values
+        path_lengths = sp.run(vertex_idx=idx_from, return_inf=True)
+        dist_matrix = path_lengths.values
 
-    end = perf_counter()
-    elapsed_time = end - start
-    logger.info(f"Edsger Dijkstra {i+1}/{repeat} - Elapsed time: {elapsed_time:8.4f} s")
+        end = perf_counter()
+        elapsed_time = end - start
+        logger.info(
+            f"Edsger Dijkstra {i+1}/{repeat} - Elapsed time: {elapsed_time:8.4f} s"
+        )
 
-    d = {
-        "library": "edsger",
-        "network": reg,
-        "trial": i,
-        "elapsed_time": elapsed_time,
-    }
-    results.append(d)
+        d = {
+            "library": "edsger",
+            "network": reg,
+            "trial": i,
+            "elapsed_time": elapsed_time,
+        }
+        results.append(d)
 
-df = pd.DataFrame.from_records(results)
-logger.info(f"Edsger min elapsed time : {df.elapsed_time.min():8.4f} s")
+    df = pd.DataFrame.from_records(results)
+    logger.info(f"Edsger min elapsed time : {df.elapsed_time.min():8.4f} s")
+
+elif lib == "GT":
+    # graph-tool
+    # ----------
+
+    logger.info("graph-tool init")
+
+    import graph_tool as gt
+    from graph_tool import topology
+
+    # create the graph
+    g = gt.Graph(directed=True)
+
+    # create the vertices
+    g.add_vertex(vertex_count)
+
+    # create the edges
+    g.add_edge_list(edges[["tail", "head"]].values)
+
+    # edge property for the travel time
+    eprop_t = g.new_edge_property("float")
+    g.edge_properties["t"] = eprop_t  # internal property
+    g.edge_properties["t"].a = edges["weight"].values
+
+    # SSSP
+
+    results = []
+    logger.info("graph-tool Run")
+    for i in range(repeat):
+        d = {}
+
+        start = perf_counter()
+
+        dist = gt.topology.shortest_distance(
+            g,
+            source=g.vertex(idx_from),
+            weights=g.ep.t,
+            negative_weights=False,
+            directed=True,
+        )
+        dist_matrix = dist.a
+
+        end = perf_counter()
+        elapsed_time = end - start
+        logger.info(
+            f"graph-tool Dijkstra {i+1}/{repeat} - Elapsed time: {elapsed_time:8.4f} s"
+        )
+
+        d = {
+            "library": "graph-tool",
+            "network": reg,
+            "trial": i,
+            "elapsed_time": elapsed_time,
+        }
+        results.append(d)
+
+    df = pd.DataFrame.from_records(results)
+    logger.info(f"graph-tool min elapsed time : {df.elapsed_time.min():8.4f} s")
+
+elif lib == "NK":
+    # NetworKit
+    # ---------
+
+    import networkit as nk
+
+    nk_file_format = nk.graphio.Format.NetworkitBinary
+    networkit_file_path = os.path.join(
+        data_dir, f"{reg}/USA-road-t.{reg}.gr.NetworkitBinary"
+    )
+
+    if os.path.exists(networkit_file_path):
+        g = nk.graphio.readGraph(networkit_file_path, nk_file_format)
+
+    else:
+        g = nk.Graph(n=vertex_count, weighted=True, directed=True, edgesIndexed=False)
+
+        for row in edges.itertuples():
+            g.addEdge(row.tail, row.head, w=row.weight)
+
+        nk.graphio.writeGraph(g, networkit_file_path, nk_file_format)
+
+    nk_dijkstra = nk.distance.Dijkstra(
+        g, idx_from, storePaths=False, storeNodesSortedByDistance=False
+    )
+
+    # SSSP
+
+    results = []
+    logger.info("NetworKit Run")
+    for i in range(repeat):
+        d = {}
+
+        start = perf_counter()
+
+        nk_dijkstra.run()
+        dist_matrix = np.asarray(nk_dijkstra.getDistances(asarray=True))
+        dist_matrix = np.where(dist_matrix >= 1.79769313e308, np.inf, dist_matrix)
+
+        end = perf_counter()
+        elapsed_time = end - start
+        logger.info(
+            f"NetworKit Dijkstra {i+1}/{repeat} - Elapsed time: {elapsed_time:8.4f} s"
+        )
+
+        d = {
+            "library": "NetworKit",
+            "network": reg,
+            "trial": i,
+            "elapsed_time": elapsed_time,
+        }
+        results.append(d)
+
+    df = pd.DataFrame.from_records(results)
+    logger.info(f"NetworKit min elapsed time : {df.elapsed_time.min():8.4f} s")
+
+# elif lib == "RX":
+#     # RustworkX
+#     # --------
+
+#     import rustworkx as rx
+
+#     graph = rx.PyDiGraph(multigraph=False)
+#     tail_indices = edges["tail"].values.tolist()
+#     head_indices = edges["head"].values.tolist()
+#     weight = edges["weight"].values.tolist()
+#     graph.extend_from_weighted_edge_list(list(zip(tail_indices, head_indices, weight)))
+
+#     # SSSP
+
+#     results = []
+#     logger.info("RustworkX Run")
+#     for i in range(repeat):
+#         d = {}
+
+#         start = perf_counter()
+
+#         start_01 = perf_counter()
+#         # edge_cost_fn = lambda x: x
+#         path_lengths = rx.dijkstra_shortest_path_lengths(
+#             graph, idx_from, edge_cost_fn=float
+#         )
+#         end_01 = perf_counter()
+#         elapsed_time_01 = end_01 - start_01
+#         logger.info(
+#             f"RustworkX step 01 - Elapsed time: {elapsed_time_01:8.4f} s"
+#         )
+
+#         # start_02 = perf_counter()
+#         # dist_matrix = np.zeros(vertex_count, dtype=np.float64)
+#         # end_02 = perf_counter()
+#         # elapsed_time_02 = end_02 - start_02
+#         # logger.info(
+#         #     f"RustworkX step 02 - Elapsed time: {elapsed_time_02:8.4f} s"
+#         # )
+
+#         # start_03 = perf_counter()
+#         # dist_matrix[list(path_lengths.keys())] = list(path_lengths.values())
+#         # end_03 = perf_counter()
+#         # elapsed_time_03 = end_03 - start_03
+#         # logger.info(
+#         #     f"RustworkX step 03 - Elapsed time: {elapsed_time_03:8.4f} s"
+#         # )
+
+#         end = perf_counter()
+#         elapsed_time = end - start
+#         logger.info(
+#             f"RustworkX Dijkstra {i+1}/{repeat} - Elapsed time: {elapsed_time:8.4f} s"
+#         )
+
+#         d = {
+#             "library": "RustworkX",
+#             "network": reg,
+#             "trial": i,
+#             "elapsed_time": elapsed_time,
+#         }
+#         results.append(d)
+
+#     df = pd.DataFrame.from_records(results)
+#     logger.info(f"RustworkX min elapsed time : {df.elapsed_time.min():8.4f} s")
+
 
 if check_result:
     logger.info(f"result check")
@@ -201,8 +400,12 @@ if check_result:
     )
     end = perf_counter()
     elapsed_time = end - start
+
     logger.info(f"SciPy Dijkstra - Elapsed time: {elapsed_time:8.4f} s")
     logger.info(f"isinf : {np.isinf(dist_matrix_ref).any()}")
     logger.info(
         f"allclose : {np.allclose(dist_matrix_ref, dist_matrix, equal_nan=True)}"
     )
+
+
+logger.info("exit")
