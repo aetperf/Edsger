fbec4bdede9c7e1e17a6a476dad82f57dc9112de
djfrancesco <pacullfrancois@gmail.com>
2023-05-22 20:15:29 +0200

    small update

---
+++ src/edsger/path_tracking.pyx
@@ -0,0 +1,16 @@
+""" 
+
+    
+author : Francois Pacull
+copyright : Architecture & Performance
+email: francois.pacull@architecture-performance.fr
+license : MIT
+"""
+
+cimport numpy as cnp
+
+
+cpdef compute_path(cnp.uint32_t[::1] vertex_indices, int vertex_idx):
+	return None
+	
+	
