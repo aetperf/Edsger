06e999e57d6b582dfe425232b47c43c42df264d5
djfrancesco <pacullfrancois@gmail.com>
2023-05-30 13:35:30 +0200

    wip

--- src/edsger/path_tracking.pyx
+++ src/edsger/path_tracking.pyx
@@ -1,5 +1,5 @@
 """ 
-
+Path tracking module.
     
 author : Francois Pacull
 copyright : Architecture & Performance
@@ -7,10 +7,62 @@ email: francois.pacull@architecture-performance.fr
 license : MIT
 """
 
+import numpy as np
 cimport numpy as cnp
 
 
-cpdef compute_path(cnp.uint32_t[::1] vertex_indices, int vertex_idx):
-	return None
-	
-	
+cpdef cnp.ndarray  compute_path(cnp.uint32_t[::1] path_links, int vertex_idx):
+    
+    cdef int path_length
+
+    path_length = compute_path_first_pass(path_links, vertex_idx)
+    path_vertices = np.empty(path_length, dtype=np.uint32)
+    compute_path_second_pass(path_links, path_vertices, vertex_idx)
+
+    return path_vertices
+    
+
+cdef int compute_path_first_pass(
+    cnp.uint32_t[::1] path_links,  
+    int vertex_idx
+) nogil:
+    """Returns the path length.
+    """
+
+    cdef:
+        size_t i, j
+        int k
+
+    # initialization
+    j = <size_t>vertex_idx
+    i = j + 1
+    k = 0
+
+    # loop
+    while i != j:
+        i = j
+        j = <size_t>path_links[j]
+        k += 1
+
+    return k
+
+cdef void compute_path_second_pass(
+    cnp.uint32_t[::1] path_links,
+    cnp.uint32_t[::1] path_vertices,  
+    int vertex_idx
+) nogil:
+
+    cdef size_t i, j, k
+
+    # initialization
+    j = <size_t>vertex_idx
+    i = j + 1
+    path_vertices[0] = j
+    k = 0
+
+    # loop
+    while i != j:
+        i = j
+        j = <size_t>path_links[j]
+        k += 1
+        path_vertices[k] = j 
