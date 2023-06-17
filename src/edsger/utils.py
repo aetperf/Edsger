edbff44bfd92d310555b4a8b039053a471a8171a
djfrancesco <pacullfrancois@gmail.com>
2023-06-17 08:52:19 +0200

    new utils module

---
+++ src/edsger/utils.py
@@ -0,0 +1,17 @@
+"""This module makes it easy to execute common tasks in Python scripts such as generate random 
+graphs.
+"""
+import numpy as np
+import pandas as pd
+
+
+def generate_random_network(n_edges=100, n_vert=20, seed=124, sort=True):
+    rng = np.random.default_rng(seed=seed)
+    tail = rng.integers(low=0, high=n_vert, size=n_edges)
+    head = rng.integers(low=0, high=n_vert, size=n_edges)
+    weight = rng.random(size=n_edges)
+    edges = pd.DataFrame(data={"tail": tail, "head": head, "weight": weight})
+    if sort:
+        edges.sort_values(by=["tail", "head"], inplace=True)
+        edges.reset_index(drop=True, inplace=True)
+    return edges
