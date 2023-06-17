4920a71201be4bdd6ccc1e08721f8c75539ac5fd
djfrancesco <pacullfrancois@gmail.com>
2023-06-17 08:54:00 +0200

    renaming

--- src/edsger/utils.py
+++ src/edsger/utils.py
@@ -5,10 +5,10 @@ import numpy as np
 import pandas as pd
 
 
-def generate_random_network(n_edges=100, n_vert=20, seed=124, sort=True):
+def generate_random_network(n_edges=100, n_verts=20, seed=124, sort=True):
     rng = np.random.default_rng(seed=seed)
-    tail = rng.integers(low=0, high=n_vert, size=n_edges)
-    head = rng.integers(low=0, high=n_vert, size=n_edges)
+    tail = rng.integers(low=0, high=n_verts, size=n_edges)
+    head = rng.integers(low=0, high=n_verts, size=n_edges)
     weight = rng.random(size=n_edges)
     edges = pd.DataFrame(data={"tail": tail, "head": head, "weight": weight})
     if sort:
