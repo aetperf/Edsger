6c48ad464c8d7f6311041e4bb7086bb15a754983
djfrancesco <francois.pacull@architecture-performance.fr>
2025-06-22 12:35:17 +0200

    added LTO flags (link time optimization)

--- setup.py
+++ setup.py
@@ -4,43 +4,50 @@ import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-extra_compile_args = ["-Ofast"]
+extra_compile_args = ["-Ofast", "-flto"]
+extra_link_args = ["-flto"]
 
 extensions = [
     Extension(
         "edsger.commons",
         ["src/edsger/commons.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
     Extension(
         "edsger.pq_4ary_dec_0b",
         ["src/edsger/pq_4ary_dec_0b.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
     Extension(
         "edsger.dijkstra",
         ["src/edsger/dijkstra.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
     Extension(
         "edsger.spiess_florian",
         ["src/edsger/spiess_florian.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
     Extension(
         "edsger.star",
         ["src/edsger/star.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
     Extension(
         "edsger.path_tracking",
         ["src/edsger/path_tracking.pyx"],
         extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     ),
 ]
