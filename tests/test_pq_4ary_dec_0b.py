24d6cb568960762a33d933ba1a1b3642855855fa
djfrancesco <francois.pacull@architecture-performance.fr>
2025-08-28 14:19:00 +0200

    pyright

--- tests/test_pq_4ary_dec_0b.py
+++ tests/test_pq_4ary_dec_0b.py
@@ -7,36 +7,36 @@ from edsger.pq_4ary_dec_0b import *  # type: ignore
 
 
 def test_init_01():
-    init_01()
+    init_01()  # type: ignore
 
 
 def test_insert_01():
-    insert_01()
+    insert_01()  # type: ignore
 
 
 def test_insert_02():
-    insert_02()
+    insert_02()  # type: ignore
 
 
 def test_insert_03():
-    insert_03()
+    insert_03()  # type: ignore
 
 
 def test_peek_01():
-    peek_01()
+    peek_01()  # type: ignore
 
 
 def test_extract_min_01():
-    extract_min_01()
+    extract_min_01()  # type: ignore
 
 
 def test_is_empty_01():
-    is_empty_01()
+    is_empty_01()  # type: ignore
 
 
 def test_decrease_key_01():
-    decrease_key_01()
+    decrease_key_01()  # type: ignore
 
 
 def test_sort_01():
-    sort_01(1000)
+    sort_01(1000)  # type: ignore
